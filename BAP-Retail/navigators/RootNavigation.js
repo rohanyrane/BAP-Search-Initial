d09487b2341aa04e8ad0bbc4cec0ce81cc6dc808
rohanyrane <ryrane.in@gmail.com>
2023-08-22 23:46:48 +0530

    Screens for Search and Search Result

--- BAP-Retail/navigators/RootNavigation.js
+++ BAP-Retail/navigators/RootNavigation.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import SearchScreen from '../screens/SearchScreen'
+import ProductScreen from '../screens/ProductScreen';
 
 export default function RootNavigation() {
     const Stack = createNativeStackNavigator()
@@ -9,7 +10,8 @@ export default function RootNavigation() {
         <NavigationContainer>
             <Stack.Navigator>
 
-                <Stack.Screen name="SearchScreen" component={SearchScreen} options={{headerShown: false}}/>
+                {/* <Stack.Screen name="SearchScreen" component={SearchScreen} options={{headerShown: false}}/> */}
+                <Stack.Screen name="ProductScreen" component={ProductScreen} options={{headerShown: false}}/>
 
             </Stack.Navigator>
         </NavigationContainer>
